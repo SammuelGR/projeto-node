e62f16debb927501941055ac3378a13104d5db2f
Sammuel Reis <sammugr@hotmail.com>
2020-06-26 01:22:17 -0300

    implements relational data between user and appointment
    creates a relation between provider_id in appointment and id in user

--- src/database/migrations/1593138813424-CreateUsers.ts
+++ src/database/migrations/1593138813424-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1593138813424 implements MigrationInterface {
 				columns: [
 					{
 						name: 'id',
-						type: 'varchar',
+						type: 'uuid',
 						isPrimary: true,
 						generationStrategy: 'uuid',
 						default: 'uuid_generate_v4()',
